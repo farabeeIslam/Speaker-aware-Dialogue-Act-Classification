3af4466630e15523e40cb8716e51a256422009f1
farabeeislam <farabeeislam@gmail.com>
2025-04-28 22:06:44 -0500

    Updated run_swda.py, added reduced dataset files, added fast sbatch script, and removed old data.zip

--- speak-turn-emb-dialog-act-clf-master/run_swda.py
+++ speak-turn-emb-dialog-act-clf-master/run_swda.py
@@ -1,29 +1,27 @@
 import os
 
-# finetune all layers, chunk size 40
-# finetune 2 layers, chunk size 240
-# finetune 1 layer, chunk size 300
-# finetune 1 layer, chunksize 160, batchsize=2
-
-
+# Fast training configuration
 if __name__ == '__main__':
     corpus = 'swda'
-    mode = ['train', 'inference'][0]
-    batch_size = 2
-    batch_size_val = 2 # don't change this number
-    emb_batch = 0
-    epochs = 5    # default 100
-    gpu = ['0,1', '', '0,1,2,3', '0,1,2,3,4,5,6,7'][0]   # default 0,1
-    lr = 1e-4   # default 1e-4
-    nlayer = 2  # default 1
-    chunk_size = 196
-    dropout = 0.5   # default 0.5
-    nfinetune = 1  # default 2
-    nclass = 43
-    speaker_info = ['none', 'emb_cls'][1]  # whether to use speaker embeddings or not
-    topic_info = ['none', 'emb_cls'][0]     # whether to use topic embeddings or not
+    mode = ['train', 'inference'][0]  # train mode
+    batch_size = 32                  # 🟰 increased batch size
+    batch_size_val = 32               # 🟰 validation batch size
+    emb_batch = 0                     # for special embedding batching, keep 0
+    epochs = 1                        # 🟰 only 1 epoch to finish fast
+    gpu = '0'                         # 🟰 use only 1 GPU (gpu id 0)
+    lr = 1e-4                         # learning rate
+    nlayer = 2                        # number of GRU layers
+    chunk_size = 128                  # chunk size for long conversations
+    dropout = 0.5                     # dropout rate
+    nfinetune = 0                     # 🟰 no BERT layers finetuned (fully frozen)
+    nclass = 43                       # number of dialogue act classes
+    speaker_info = 'emb_cls'           # use speaker embeddings
+    topic_info = 'none'                # do not use topic embeddings
+
+    # Make result directory if it doesn't exist
     os.makedirs(f'results_{corpus}', exist_ok=True)
 
+    # Build the result file name
     file_name = f"results_{corpus}/{corpus}_chunk={chunk_size}_nlayer={nlayer}.txt"
 
     if nfinetune != 2:
@@ -31,7 +29,6 @@ if __name__ == '__main__':
 
     if speaker_info != 'none':
         file_name = file_name[:-4] + f'_speaker={speaker_info}.txt'
-
 
     if topic_info != 'none':
         file_name = file_name[:-4] + f'_tinfo={topic_info}.txt'
@@ -49,23 +46,12 @@ if __name__ == '__main__':
     if n_gpu != 2:
         file_name = file_name[:-4] + f'_ngpu={n_gpu}.txt'
 
-    # bash_file = ''
-    # with open(f'run_{corpus}.sh') as f:
-    #     for line in f:
-    #         str_line = list(line)
-    #         if 'gpu' in line:
-    #             pos = line.index('=')
-    #             str_line[pos+1:pos+2] = str(n_gpu)
-    #         bash_file += ''.join(str_line)
-    # with open(f'run_{corpus}.sh', 'w') as f:
-    #     f.write(bash_file)
+    # Create the command string to run engine.py
+    command = f"python -u engine.py --corpus={corpus} --mode={mode} --gpu={gpu} " \
+              f"--batch_size={batch_size} --batch_size_val={batch_size_val} " \
+              f"--epochs={epochs} --lr={lr} --nlayer={nlayer} --chunk_size={chunk_size} " \
+              f"--dropout={dropout} --nfinetune={nfinetune} --speaker_info={speaker_info} " \
+              f"--topic_info={topic_info} --nclass={nclass} --emb_batch={emb_batch}"
 
-    command = f"python -u engine.py --corpus={corpus} --mode={mode} --gpu={gpu} --batch_size={batch_size} " \
-              f"--batch_size_val={batch_size_val} --epochs={epochs} " \
-              f"--lr={lr} --nlayer={nlayer} --chunk_size={chunk_size} --dropout={dropout} " \
-              f"--nfinetune={nfinetune} --speaker_info={speaker_info} " \
-              f"--topic_info={topic_info} --nclass={nclass} --emb_batch={emb_batch}" \
-              # f" > {file_name}"
-
-    print(command)
-    os.system(command)
+    print(command)  # show the command that will be executed
+    os.system(command)  # execute the command
